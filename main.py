1ef01999489dc183a52d2adaf2938120b6553be0
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-07 15:47:52 +0900

    플레이어에 따라 변경 완료

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import requests
 from bs4 import BeautifulSoup as bs
-
-def UpDownGame() :
+player_turn = 1
+def UpDownGame(playerName) :
     titles = []
     url = "https://www.premierleague.com/tables"
     response = requests.get(url)
@@ -12,35 +12,81 @@ def UpDownGame() :
     for i in range(20):
         titles.append(result[i].getText())
     totalNum = 20
-    title_answer = titles[random.randint(0,totalNum-1)]
-    player_num = 4
-
-    now = 1
-    startPoint = 1
-    endPoint = 20
-    print (title_answer + "의 등수를 맞춰보자 !")
-    while now < player_num:
-        now = now +1
-        guess_order = random.randint(startPoint, endPoint)
-        print (startPoint, endPoint)
-        print ("예상하는 등수는?" + str(guess_order))
-        if (guess_order > titles.index(title_answer)+1):
-            print ("Up!")
-            endPoint = guess_order-1
-        elif guess_order < titles.index(title_answer)+1:
-            print ("Down!")
-            startPoint = guess_order+1
+    guess_order = 0
+    if (playerName == "Player"):
+        teamName = ""
+        while teamName not in titles:  # 올바른 팀 이름이 입력될 때까지 반복
+            teamName = input("순위를 맞출 팀을 말하세요!: ")
+            if teamName not in titles:
+                print("잘못된 팀 이름입니다! 다시 입력해주세요.")
+        player_num = 4
+        now = 1
+        startPoint = 1
+        endPoint = 20
+        print (teamName + "의 등수를 맞춰보자 !")
+        while now < player_num:
+            now = now +1
+            guess_order = random.randint(startPoint, endPoint)
+            print (startPoint, endPoint)
+            print ("예상하는 등수는?" + str(guess_order))
+            if (guess_order > titles.index(teamName)+1):
+                print ("Up!")
+                endPoint = guess_order-1
+            elif guess_order < titles.index(teamName)+1:
+                print ("Down!")
+                startPoint = guess_order+1
+            else:
+                print ("정답!")
+                break
+        if guess_order == titles.index(teamName)+1 or now < player_num:
+            #출제자가 마신다
+            print(1)
         else:
-            print ("정답!")
-            break
-            
-
-    if guess_order == titles.index(title_answer)+1 or now < player_num:
-        #출제자가 마신다
-        print(1)
+            print(2)
+            #나머지가 마신다
     else:
-        print(2)
-        #나머지가 마신다
+        teamName = titles[random.randint(0,totalNum-1)]
+        player_num = 4
+
+        now = 1
+        startPoint = 1
+        endPoint = 20
+        print (teamName + "의 등수를 맞춰보자 !")
+        while now < player_num:
+            if now==player_turn:
+                now = now + 1
+                guess_order = int(input("예상하는 등수는? "))
+                if (guess_order > titles.index(teamName)+1):
+                    print ("Up!")
+                    endPoint = guess_order-1
+                elif guess_order < titles.index(teamName)+1:
+                    print ("Down!")
+                    startPoint = guess_order+1
+                else:
+                    print ("정답!")
+                    break
+            else:  
+                now = now +1
+                guess_order = random.randint(startPoint, endPoint)
+                print (startPoint, endPoint)
+                print ("예상하는 등수는?" + str(guess_order))
+                if (guess_order > titles.index(teamName)+1):
+                    print ("Up!")
+                    endPoint = guess_order-1
+                elif guess_order < titles.index(teamName)+1:
+                    print ("Down!")
+                    startPoint = guess_order+1
+                else:
+                    print ("정답!")
+                    break
+        if guess_order == titles.index(teamName)+1 or now < player_num:
+            #출제자가 마신다
+            print(1)
+        else:
+            print(2)
+            #나머지가 마신다
+
+    
 
 
-UpDownGame()
+UpDownGame("Players")
